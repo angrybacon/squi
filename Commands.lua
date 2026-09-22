9b4f15868759307dc4e42a74d388c27c5d8858a5
Mathieu Marques <mathieumarques78@gmail.com>
2026-09-22 22:29:12 +0200

    feat(commands): enable input tokens in /colors

--- Commands.lua
+++ Commands.lua
@@ -1,16 +1,39 @@
 SLASH_COLORS1 = "/colors"
 
-SlashCmdList["COLORS"] = function()
-  local labels = {}
+---@param input string
+SlashCmdList["COLORS"] = function(input)
+  local tokens = {}
+
+  for token in input:gmatch("%S+") do
+    tokens[#tokens + 1] = token:lower()
+  end
+
+  local names = {}
+
   for name, value in pairs(_G) do
-    local label = type(name) == "string" and name:match("^(.+)_FONT_COLOR$")
-    if label and type(value) == "table" and type(value.r) == "number" then
-      labels[#labels + 1] = label
+    if
+      type(name) == "string"
+        and name:find("_COLOR$")
+        and type(value) == "table"
+        and type(value.r) == "number"
+    then
+      local matches = true
+      for _, token in ipairs(tokens) do
+        if not name:lower():find(token, 1, true) then
+          matches = false
+          break
+        end
+      end
+      if matches then
+        names[#names + 1] = name
+      end
     end
   end
-  table.sort(labels)
-  for _, label in ipairs(labels) do
-    local c = _G[label .. "_FONT_COLOR"]
-    print(CreateColor(c.r, c.g, c.b, c.a or 1):WrapTextInColorCode(label))
+
+  table.sort(names)
+
+  for _, name in ipairs(names) do
+    local c = _G[name]
+    print(CreateColor(c.r, c.g, c.b, c.a or 1):WrapTextInColorCode(name))
   end
 end
