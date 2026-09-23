1ddfcb3f75da08b55fcb7045267d5b539ac3073a
Mathieu Marques <mathieumarques78@gmail.com>
2026-09-23 08:16:27 +0200

    feat(defaults): configure the interface scale

---
+++ AGENTS.md
@@ -0,0 +1,10 @@
+# Game Directory
+
+World of Warcraft is installed at `~/Games/battlenet/drive_c/Program Files
+(x86)/World of Warcraft`.
+
+# Typings
+
+The vscode-wow-api annotations used for typing are at
+`~/Workspace/vscode-wow-api`. They might be outdated, ask the user to pull new
+code if that is found to be true.
--- modules/Defaults.lua
+++ modules/Defaults.lua
@@ -1,15 +1,19 @@
 ---@type string, Squi
 local _, S = ...
 
+local SCALE = 0.8
 local VOLUME_HIGH = 1.0
 local VOLUME_LOW = 0.1
 local VOLUME_MEDIUM = 0.5
 
 table.insert(S.Modules, function()
+  SetCVar("RenderScale", 1.0)
   SetCVar("Sound_AmbienceVolume", VOLUME_MEDIUM)
   SetCVar("Sound_DialogVolume", VOLUME_HIGH)
   SetCVar("Sound_EnableSoundWhenGameIsInBG", "1")
   SetCVar("Sound_MasterVolume", 1.0)
   SetCVar("Sound_MusicVolume", VOLUME_LOW)
   SetCVar("Sound_SFXVolume", VOLUME_HIGH)
+  SetCVar("uiScale", SCALE)
+  SetCVar("useUiScale", "1")
 end)
